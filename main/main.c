be5a1f0a52583a17fe3958d08e97ac4e05953ad9
mehmet <54444153+mehmetgok1@users.noreply.github.com>
2024-11-25 12:27:55 +0300

    Add files via upload

---
+++ main/main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_timer.h"
+#include "ui.h"
+
+void app_main(void){  
+    
+    init_mng();
+    init_ui();
+}
+
